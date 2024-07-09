4affabdb1a13bd2d5cd4ef793d30994ff88d6793
The-Nikparmar <parmarnik1015@gmail.com>
2024-07-09 16:02:17 +0530

    done

--- HRMS/settings.py
+++ HRMS/settings.py
@@ -13,14 +13,14 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 import os
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 
 # Quick-start development settings - unsuitable for production
